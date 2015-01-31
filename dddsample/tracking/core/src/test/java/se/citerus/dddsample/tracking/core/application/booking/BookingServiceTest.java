0c44fe886f4705b89c447908c27b4a190846e3de
btforsythe <brian.t.forsythe@gmail.com>
2015-01-31 18:00:31 -0500

    updated, cleaned up test

--- dddsample/tracking/core/src/test/java/se/citerus/dddsample/tracking/core/application/booking/BookingServiceTest.java
+++ dddsample/tracking/core/src/test/java/se/citerus/dddsample/tracking/core/application/booking/BookingServiceTest.java
@@ -1,53 +1,98 @@
 package se.citerus.dddsample.tracking.core.application.booking;
 
-import junit.framework.TestCase;
-import static org.easymock.EasyMock.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static se.citerus.dddsample.tracking.core.domain.model.location.SampleLocations.*;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
+
 import se.citerus.dddsample.tracking.core.domain.model.cargo.Cargo;
 import se.citerus.dddsample.tracking.core.domain.model.cargo.CargoRepository;
 import se.citerus.dddsample.tracking.core.domain.model.cargo.TrackingId;
 import se.citerus.dddsample.tracking.core.domain.model.cargo.TrackingIdFactory;
+import se.citerus.dddsample.tracking.core.domain.model.location.Location;
 import se.citerus.dddsample.tracking.core.domain.model.location.LocationRepository;
-import static se.citerus.dddsample.tracking.core.domain.model.location.SampleLocations.CHICAGO;
-import static se.citerus.dddsample.tracking.core.domain.model.location.SampleLocations.STOCKHOLM;
 import se.citerus.dddsample.tracking.core.domain.model.location.UnLocode;
 import se.citerus.dddsample.tracking.core.domain.service.RoutingService;
 
-import java.util.Date;
+// TODO this test results in log messages -- a mock needs to be injected somewhere to avoid this
+public class BookingServiceTest {
 
-public class BookingServiceTest extends TestCase {
+	// TODO this should be BookingServiceImplTest, not BookingServiceTest
+	private BookingServiceImpl underTest;
 
-  BookingServiceImpl bookingService;
-  CargoRepository cargoRepository;
-  LocationRepository locationRepository;
-  RoutingService routingService;
-  TrackingIdFactory trackingIdFactory;
+	@Mock
+	private RoutingService routingService;
+	@Mock
+	private TrackingIdFactory trackingIdFactory;
+	@Mock
+	private CargoRepository cargoRepository;
+	@Mock
+	private LocationRepository locationRepository;
 
-  protected void setUp() throws Exception {
-    cargoRepository = createMock(CargoRepository.class);
-    locationRepository = createMock(LocationRepository.class);
-    routingService = createMock(RoutingService.class);
-    trackingIdFactory = createMock(TrackingIdFactory.class);
-    bookingService = new BookingServiceImpl(routingService, trackingIdFactory, cargoRepository, locationRepository);
-  }
+	@Mock
+	private Date arrivalDeadline;
 
-  public void testRegisterNew() {
-    TrackingId expectedTrackingId = new TrackingId("TRK1");
-    UnLocode fromUnlocode = new UnLocode("USCHI");
-    UnLocode toUnlocode = new UnLocode("SESTO");
+	@Before
+	public void setup() throws Exception {
 
-    expect(trackingIdFactory.nextTrackingId()).andReturn(expectedTrackingId);
-    expect(locationRepository.find(fromUnlocode)).andReturn(CHICAGO);
-    expect(locationRepository.find(toUnlocode)).andReturn(STOCKHOLM);
+		MockitoAnnotations.initMocks(this);
 
-    cargoRepository.store(isA(Cargo.class));
+		underTest = new BookingServiceImpl(routingService, trackingIdFactory,
+				cargoRepository, locationRepository);
 
-    replay(cargoRepository, locationRepository, trackingIdFactory);
+		stubLocationRetrieval();
+	}
 
-    TrackingId trackingId = bookingService.bookNewCargo(fromUnlocode, toUnlocode, new Date());
-    assertEquals(expectedTrackingId, trackingId);
-  }
+	private void stubLocationRetrieval() {
+		whenDereferencing(chicagoLocationCode()).thenReturn(CHICAGO);
+		whenDereferencing(stockholmLocationCode()).thenReturn(STOCKHOLM);
+	}
 
-  protected void tearDown() throws Exception {
-    verify(cargoRepository, locationRepository, trackingIdFactory);
-  }
+	private OngoingStubbing<Location> whenDereferencing(UnLocode code) {
+		return when(locationRepository.find(code));
+	}
+
+	// TODO Would it be a good idea to create location code constants for use in
+	// tests?
+	private UnLocode chicagoLocationCode() {
+		return new UnLocode("USCHI");
+	}
+
+	private UnLocode stockholmLocationCode() {
+		return new UnLocode("SESTO");
+	}
+
+	@Test
+	public void shouldRegisterNew() {
+
+		// TODO put tracking id is method. will break atm because it doesn't
+		// implement equals
+		whenRetrievingNextTrackingId().thenReturn(expectedTrackingId());
+
+		TrackingId result = underTest.bookNewCargo(chicagoLocationCode(),
+				stockholmLocationCode(), arrivalDeadline);
+		assertThat(result, is(expectedTrackingId()));
+
+		// TODO any is bad. need to separate logic and construction in impl
+		verify(cargoRepository).store(Mockito.any(Cargo.class));
+	}
+
+	private OngoingStubbing<TrackingId> whenRetrievingNextTrackingId() {
+		return when(trackingIdFactory.nextTrackingId());
+	}
+
+	private TrackingId expectedTrackingId() {
+		// TODO we should simply make this class not final and mock this
+		return new TrackingId("TRK1");
+	}
+
 }
